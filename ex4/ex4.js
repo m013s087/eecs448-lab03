55d709f7b855df73cf15cc03bffb1f328c63aef3
Matthew Showers <MDSTardis42@gmail.com>
2018-09-24 10:18:15 -0500

    brief flash of border

---
+++ ex4/ex4.js
@@ -0,0 +1,12 @@
+"use strict"
+function change()
+{
+	let borderColor = document.getElementById("borderColor").value;
+	let Width = document.getElementById("borderWidth").value;
+	let backColor = document.getElementById("backColor").value;
+	let p=document.getElementById("target")
+	p.style.borderStyle="solid"
+	p.style.borderColor=borderColor;
+	p.style.borderWidth=width;
+	p.style.backgroundColor=backColor;
+}
