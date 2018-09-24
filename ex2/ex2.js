75213c1d74d8d918d2b00a5eb5547421671231f9
Matthew Showers <MDSTardis42@gmail.com>
2018-09-24 13:54:31 -0500

    exercise 2 complete

--- ex2/ex2.js
+++ ex2/ex2.js
@@ -1,12 +1,12 @@
 "use strict"
 let now = 1;
-let slide = Document.getElementById("pic");
-let val = ["img1.jpeg","img2.png","img3.jpg","img4.jpg","img5.png"]
-let total = val.length
+let val = ["img1.jpeg","img2.png","img3.jpg","img4.jpg","img5.png"];
+let total = 5;
+
 function next()
 {
 	
-	if(now == total)
+	if(now === total)
 	{
 		now = 1;
 	}
@@ -19,7 +19,7 @@ function next()
 
 function prev()
 {
-	if(now == 1)
+	if(now === 1)
 	{
 		now =  total;
 	}
@@ -27,11 +27,10 @@ function prev()
 	{
 		now--;
 	}
-	change()
+	change();
 }
 
 function change()
 {
-	// alert("NOW: "+ now);
-	slide.src=val[now-1];
+	document.getElementById("pic").src=val[now-1];
 }
